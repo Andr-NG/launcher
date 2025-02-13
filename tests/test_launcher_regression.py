233675eb04ee9841facfcb0263fbf7953736a911
AndrNGVN <andrey.nguyen@multilogin.com>
2025-02-13 17:40:19 +0700

    unskiping tests

--- tests/test_launcher_regression.py
+++ tests/test_launcher_regression.py
@@ -31,7 +31,7 @@ class TestLauncherRegression:
         assert response.status.http_code == 200, "Failed sign-in attempt"
         logger.info(f"Finishing {request.node.name}")
 
-    # @pytest.mark.skip(reason="Skipping this test for now.")
+    @pytest.mark.skip(reason="Skipping this test for now.")
     def test_get_launcher_version(
         self, request: FixtureRequest, launcher_api: API.Launcher
     ) -> None:
@@ -42,7 +42,7 @@ class TestLauncherRegression:
         assert response.data.env == os.getenv("ENV")
         logger.info(f"Finishing {request.node.name}")
 
-    # @pytest.mark.skip(reason="Skipping this test for now.")
+    @pytest.mark.skip(reason="Skipping this test for now.")
     def test_ws_connection(
         self, request: FixtureRequest, launcher_api: API.Launcher
     ) -> websocket.WebSocket | None:
@@ -101,7 +101,7 @@ class TestLauncherRegression:
         assert parsed["Profiles"][0]["Status"] == "start_browser"
         ws.close()
 
-    # @pytest.mark.skip(reason="Skipping this test for now")
+    @pytest.mark.skip(reason="Skipping this test for now")
     def test_get_folder_id(self, request: FixtureRequest, mlx_api: API.MLX) -> None:
         logger.info(f"Executing {request.node.name}")
         response = mlx_api.get_folder_id()
@@ -109,7 +109,7 @@ class TestLauncherRegression:
         assert response.status.http_code == 200, 'Failed to retrieve folder_id'
         logger.info(f"Finishing {request.node.name}")
 
-    # @pytest.mark.skip(reason="Skipping this test for now.")
+    @pytest.mark.skip(reason="Skipping this test for now.")
     def test_create_profile(self, mlx_api: API.MLX, request: FixtureRequest) -> None:
         logger.info(f"Executing {request.node.name}")
         body = data.PROFILE_GENERIC
